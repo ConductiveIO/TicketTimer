689caa700aeff2365bd86b5ba2090b294e68f7ce
conductiveio <grodin.robby@gmail.com>
2015-02-01 18:05:56 -0500

    Added collections and selection logic

--- tasks.js
+++ tasks.js
@@ -1,11 +1,62 @@
 Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
-  Template.hello.helpers({
+  // Return tasks from DB
+  Template.body.helpers({
     tasks: function () {
       return Tasks.find({});
     }
   });
+  
+  // Set up entry form
+  Template.body.events({
+    "submit .task-entry": function (event) {
+
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        name: text,
+        time: 0
+      });
+
+      // Clear form
+      event.target.text.value = "";
+
+      // Prevent default form submit
+      return false;
+    }
+  }); 
+
+  // Timer
+  var timer = $('#timer'),
+    clock, offset;
+
+  function start() {
+    offset   = Date.now();
+    interval = setInterval(update, options.delay);
+  }
+
+  function update() {
+    clock += delta();
+    render();
+  }
+  
+  function render() {
+    timer.innerHTML = clock/1000; 
+  }
+
+  // Attach click events
+  Template.task_list.events({
+    "click .task-icon": function(event) {
+      console.log($(this._id));
+      $('.task-icon').removeClass('selected');
+      $('#' + this._id).addClass('selected');
+    },
+    "click .remove-btn": function() {
+      Tasks.remove(this._id);
+    }
+  });
+}
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
